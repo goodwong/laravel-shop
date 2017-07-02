1e56d64fccd80e84c67971e2bdc8f404bfd32167
william <goodwong@foxmail.com>
2017-07-02 02:47:32 +0000

    update schema

--- database/migrations/2017_06_25_085716_create_orders_table.php
+++ database/migrations/2017_06_25_085716_create_orders_table.php
@@ -15,32 +15,20 @@ class CreateOrdersTable extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('context', 32)->nullable()
-            ->comment('如：shop_15、project_15、recharge');
-            $table->integer('user_id')->unsigned()
-            ->comment('用户id');
-            $table->jsonb('contacts')->nullable()
-            ->comment('联系人信息');
-            $table->string('comment')->nullable()
-            ->comment('用户评论');
-            $table->string('status', 16)->default('new')
-            ->comment('订单状态');
-            $table->integer('subtotal')->unsigned()->default(0)
-            ->comment('小计：不含运费、税');
-            $table->integer('grand_total')->unsigned()->default(0)
-            ->comment('总计');
-            $table->integer('shipping_amount')->unsigned()->default(0)
-            ->comment('运费');
-            $table->integer('discount_amount')->unsigned()->default(0)
-            ->comment('折扣');
-            $table->integer('paid_total')->unsigned()->default(0)
-            ->comment('已支付金额');
-            $table->jsonb('records')->nullable()
-            ->comment('订单动态记录');
-            $table->timestamp('expected_at')->nullable()
-            ->comment('预约时间');
-            $table->timestamp('finished_at')->nullable()
-            ->comment('实际完成时间');
+            $table->string('context', 32)->nullable()->comment('如：shop_15、project_15、recharge');
+            $table->integer('user_id')->unsigned()->comment('用户id');
+            $table->jsonb('contacts')->nullable()->comment('联系人信息');
+            $table->string('comment')->nullable()->comment('用户评论');
+            $table->string('status', 16)->default('new')->comment('订单状态');
+            $table->string('currency', 16)->default('CNY')->comment('币种，默认 CNY');
+            $table->integer('subtotal')->unsigned()->default(0)->comment('小计：不含运费、税');
+            $table->integer('grand_total')->unsigned()->default(0)->comment('总计');
+            $table->integer('shipping_amount')->unsigned()->default(0)->comment('运费');
+            $table->integer('discount_amount')->unsigned()->default(0)->comment('折扣');
+            $table->integer('paid_total')->unsigned()->default(0)->comment('已支付金额');
+            $table->jsonb('records')->nullable()->comment('订单动态记录');
+            $table->timestamp('expected_at')->nullable()->comment('预约时间');
+            $table->timestamp('finished_at')->nullable()->comment('实际完成时间');
             $table->timestamps();
             $table->softDeletes();
         });
---
+++ database/migrations/2017_06_27_162350_create_order_items_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrderItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order_items', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('type', 16)->comment('区分业务逻辑');
+            $table->integer('order_id')->unsigned()->comment('订单id');
+            $table->string('shop_id', 16)->nullable()->comment('商家id，如 15 或service_plan');
+            $table->string('product_id', 16)->nullable()->comment('产品id，如 2837，或plan_B等');
+            $table->string('group', 16)->nullable()->comment('用于展示时的分组，如：“五金店”、“配件”，系统会按group排序');
+            $table->string('name', 32)->comment('产品名称');
+            $table->string('sku', 32)->nullable()->comment('商品统一编码');
+            $table->integer('price')->nullable()->comment('原始单价，不含选项、不含折扣、锐、运费等，有可能为负数');
+            $table->integer('qty')->nullable()->comment('数量，不计数量为null，如coupon“业主推荐 -500元”');
+            $table->integer('row_total')->nullable()->comment('行小计，如装修里的特殊要求，免费');
+            $table->string('comment')->nullable()->comment('用户留言');
+            $table->jsonb('data')->nullable()->comment('数据，根据type定义');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order_items');
+    }
+}
