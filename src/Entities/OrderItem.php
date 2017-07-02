f8d2f6a71b8066235f52ced034188636f46f4ed3
william <goodwong@foxmail.com>
2017-07-02 03:22:14 +0000

    add OrderItem

---
+++ src/Entities/OrderItem.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Goodwong\LaravelShop\Entities;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class OrderItem extends Model
+{
+    use SoftDeletes;
+
+    /**
+     * table name
+     */
+    protected $table = 'order_items';
+
+    /**
+     * fillable fields
+     */
+    protected $fillable = [
+        'type',
+        'order_id',
+        'shop_id',
+        'product_id',
+        'group',
+        'name',
+        'sku',
+        'price',
+        'qty',
+        'row_total',
+        'comment',
+        'data',
+    ];
+    
+    /**
+     * date
+     */
+    protected $dates = [
+        'deleted_at',
+    ];
+
+    /**
+     * cast attributes
+     */
+    protected $casts = [
+        'data' => 'object',
+    ];
+
+    //
+}
