59da8e724ca6aa1dd150105c93369ddbd6ffe330
william <goodwong@foxmail.com>
2017-11-13 18:26:54 +0800

    add Product Entity

---
+++ database/migrations/2017_11_10_080447_create_products_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('shop_products', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('shop_id', 16)->nullable()->comment('商家id，如 15 或service_plan');
+            $table->string('category_id', 32)->nullable()->comment('分类，如 shop_3_accessories');
+            $table->string('name', 32)->comment('产品名称');
+            $table->string('sku', 32)->nullable()->comment('商品统一编码');
+            $table->integer('price')->comment('原始单价，不含选项、不含折扣、锐、运费等，有可能为负数');
+            $table->integer('position')->default(0)->comment('产品排序用，整数，越小越前面');
+            $table->jsonb('settings')->nullable()->comment('自定义');
+            $table->jsonb('options')->nullable()->comment('选项');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('shop_products');
+    }
+}
---
+++ src/Entities/Product.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Goodwong\LaravelShop\Entities;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Product extends Model
+{
+    use SoftDeletes;
+
+    /**
+     * table name
+     */
+    protected $table = 'shop_products';
+
+    /**
+     * fillable fields
+     */
+    protected $fillable = [
+        'shop_id',
+        'category_id',
+        'name',
+        'sku',
+        'price',
+        'position',
+        'settings',
+        'options',
+    ];
+    
+    /**
+     * date
+     */
+    protected $dates = [
+        'deleted_at',
+    ];
+
+    /**
+     * cast attributes
+     */
+    protected $casts = [
+        'settings' => 'object',
+        'options' => 'array',
+    ];
+
+    //
+}
