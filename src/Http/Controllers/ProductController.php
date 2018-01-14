893ab7255a737d048ffe0faeeccdb37a78f0fe88
William <goodwong@foxmail.com>
2018-01-14 17:44:05 +0800

    add product:store/update api

--- src/Http/Controllers/ProductController.php
+++ src/Http/Controllers/ProductController.php
@@ -48,7 +48,8 @@ class ProductController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $product = Product::create($request->all());
+        return $product;
     }
 
     /**
@@ -59,7 +60,7 @@ class ProductController extends Controller
      */
     public function show(Product $product)
     {
-        //
+        return $product;
     }
 
     /**
@@ -82,7 +83,8 @@ class ProductController extends Controller
      */
     public function update(Request $request, Product $product)
     {
-        //
+        $product->update($request->all());
+        return $product;
     }
 
     /**
@@ -93,6 +95,6 @@ class ProductController extends Controller
      */
     public function destroy(Product $product)
     {
-        //
+        return $product->delete();
     }
 }
